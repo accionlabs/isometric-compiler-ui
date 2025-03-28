92d0c14e0a1e472717d207b55a661b47e0ce8c9a
pranavaccion <pranav.patil@accionlabs.com>
2025-03-28 10:33:54 +0530

    minor fix

--- src/components/ui/login.tsx
+++ src/components/ui/login.tsx
@@ -20,7 +20,7 @@ export default function Login() {
             </video>
             <div className="flex flex-col mb-20">
                 <p className=" text-white text-2xl">Welcome to</p>
-                <h1 className="text-3xl font-bold text-white">Breeze.AI</h1>
+                <h1 className="text-5xl font-bold text-white">Breeze.AI</h1>
 
                 <button
                     className="bg-blue-700 px-4 py-2 mt-16 rounded text-white font-bold"
