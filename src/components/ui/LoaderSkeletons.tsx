f472fc64bdb1d58f5c7e14a0e19dd92fa8c2ad1f
pranavaccion <pranav.patil@accionlabs.com>
2025-04-16 18:02:03 +0530

    bg color chnage of loader

--- src/components/ui/LoaderSkeletons.tsx
+++ src/components/ui/LoaderSkeletons.tsx
@@ -2,7 +2,7 @@ import SVGPreview from "./SVGPreview";
 
 export function Loader() {
     return (
-        <div className="flex justify-center items-center h-screen bg-customBlack w-screen">
+        <div className="flex justify-center items-center h-screen bg-customGray w-screen">
             <div className="w-12 h-12 border-4 border-blue-700 border-t-transparent border-b-transparent rounded-full animate-spin"></div>
         </div>
     );
